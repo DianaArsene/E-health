ac33f91a792579e8e0b381eab087690d7512f152
diana011 <dianaelena011@gmail.com>
2018-06-12 16:00:38 +0300

    Pagina pt doctori, nefacuta

---
+++ doctori.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<link rel="stylesheet" href="assets/css/styleAsistenti.css" type="text/css">
+	<script type="text/javascript" src="assets/js/libs/jquery-3.3.1.min.js"></script>	
+
+	<!-- BOOTSTRAP CDN -->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!--END -->
+</head>
+<body>
+	<div class="card">
+		<img src="doctor.jpg" class="img-fluid imgBanner2">
+		<div class="form paddingClass">
+			<form class="login-form" method="post">
+				
+				
+			</form>
+		</div>
+	</div>
+</div>
+</body>
+</html>
