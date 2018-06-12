5e1c455b409f395968c78b0d825cf7146288fa2c
diana011 <dianaelena011@gmail.com>
2018-06-12 22:20:33 +0300

    Add files via upload

---
+++ helperUpdate.php
@@ -0,0 +1,12 @@
+<?php
+// connect to the database
+	$db = mysqli_connect('localhost', 'root', '', 'ehealth');
+	$id = $_POST['Id'];
+
+	$res = mysqli_query($db,"UPDATE utilizatori SET Status=1 WHERE Id='$id'");
+	if($res) {
+		echo 'Success';
+	} else {
+		echo 'Err';
+	}
+?>
