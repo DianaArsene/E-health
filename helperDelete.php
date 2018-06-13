6ad77966f5fa3b0bbf21450eefd877052ffb7367
diana011 <dianaelena011@gmail.com>
2018-06-13 08:58:21 +0300

    Update helperDelete.php

--- helperDelete.php
+++ helperDelete.php
@@ -1,13 +1,19 @@
 <?php
 // connect to the database
 	$db = mysqli_connect('localhost', 'root', '', 'ehealth');
-	$id = $_POST['Id'];
+	
 
-	$res = mysqli_query($db,"DELETE FROM utilizatori WHERE Id='$id'");
-	if($res) {
-		echo 'Success';
-	} else {
-		echo 'Err';
+	if (isset($_POST['Id'])) {
+        $idStr = $_POST['Id'];
+        $id = $idStr[0];
+        echo $id;
+    	$res = mysqli_query($db,"DELETE FROM utilizatori WHERE Id='$id'");
+    		if($res) {
+    			echo 'Success';
+                header("Refresh:0");
+    		} else {
+    			echo 'Err';
+                header("Refresh:0");
+    		}
 	}
-
 ?>
